484470ba4953bdb400c0e82c0a336eb295a674f4
Vuong Nguyen <nguyenvanvuong972@gmail.com>
2019-09-12 01:00:23 +0700

    ver 1.0

---
+++ ResetTester/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ResetTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lb_TimeDisplay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_TimeDisplay
+            // 
+            this.lb_TimeDisplay.AutoSize = true;
+            this.lb_TimeDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_TimeDisplay.Location = new System.Drawing.Point(32, 10);
+            this.lb_TimeDisplay.Name = "lb_TimeDisplay";
+            this.lb_TimeDisplay.Size = new System.Drawing.Size(24, 25);
+            this.lb_TimeDisplay.TabIndex = 0;
+            this.lb_TimeDisplay.Text = "5";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(113, 9);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(118, 23);
+            this.btn_Stop.TabIndex = 1;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 44);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.lb_TimeDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_TimeDisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_Stop;
+    }
+}
+
---
+++ ResetTester/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ResetTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count = 5;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lb_TimeDisplay.Text = count.ToString();
+            count--;
+            if(count == 0)
+            {
+                Process.Start("shutdown", "/s /t 0");
+                timer1.Stop();
+                this.Close();
+            }
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            if(this.btn_Stop.Text == "Start")
+            {
+                timer1.Start();
+                this.btn_Stop.Text = "Stop";
+            }
+                else
+            {
+                timer1.Stop();
+                this.btn_Stop.Text = "Start";
+            }
+        
+        }
+    }
+}
